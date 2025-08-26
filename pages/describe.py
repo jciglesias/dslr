e1da034bd9671a557658f811bc0cf35d48ff8924
Juan carlos IGLESIAS GONZALEZ <jiglesia@f2r8s6.clusters.42paris.fr>
2025-08-26 14:59:29 +0200

    using stats_table

--- pages/describe.py
+++ pages/describe.py
@@ -1,9 +1,10 @@
 import streamlit as st
 import pandas as pd
+from stats.table_stats import stats_table
 
 csv_data = st.file_uploader("Upload your CSV file", type=["csv"])
 
 if csv_data is not None:
     df = pd.read_csv(csv_data)
-    described_data = df.describe().T.reset_index() # change to new function
+    described_data = stats_table(df)
     st.dataframe(described_data)
