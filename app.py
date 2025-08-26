bf5e9ae7ba0bf3fc919bf95707eba36f1b354ab4
Juan carlos IGLESIAS GONZALEZ <jiglesia@f2r8s6.clusters.42paris.fr>
2025-08-26 15:28:13 +0200

    remove postgres and use session_state for df

--- app.py
+++ app.py
@@ -1,3 +1,13 @@
 import streamlit as st
+import pandas as pd
 
-st.write("analyze dataset")
+st.set_page_config(page_title="DSLR App", page_icon=":bar_chart:", layout="wide")
+st.title("DSLR App")
+
+if "df" not in st.session_state:
+    csv_data = st.file_uploader("Upload your CSV file", type=["csv"])
+    if csv_data is not None:
+        st.session_state.df = pd.read_csv(csv_data)
+
+if 'df' in st.session_state and st.session_state.df is not None:
+    st.dataframe(st.session_state.df)
--- pages/describe.py
+++ pages/describe.py
@@ -2,9 +2,12 @@ import streamlit as st
 import pandas as pd
 from stats.table_stats import stats_table
 
-csv_data = st.file_uploader("Upload your CSV file", type=["csv"])
+# csv_data = st.file_uploader("Upload your CSV file", type=["csv"])
 
-if csv_data is not None:
-    df = pd.read_csv(csv_data)
+if "df" in st.session_state:
+    df = st.session_state.df
     described_data = stats_table(df)
     st.dataframe(described_data)
+
+else:
+    st.write("Please upload a CSV file in the main app to see the data description.")
