bf268ffd9fb0be8bbf2228dbd2c0fb0332655c06
Juan carlos IGLESIAS GONZALEZ <jiglesia@f2r8s6.clusters.42paris.fr>
2025-08-26 14:26:34 +0200

    show only table only after reading csv

--- pages/describe.py
+++ pages/describe.py
@@ -6,13 +6,4 @@ csv_data = st.file_uploader("Upload your CSV file", type=["csv"])
 if csv_data is not None:
     df = pd.read_csv(csv_data)
     described_data = df.describe().T.reset_index() # change to new function
-else:
-    described_data = {
-        'statistics': ['count','mean', 'std', 'min','P25', 'P50', 'P75', 'max'],
-        'Feature1': range(8),
-        'Feature2': range(10,18),
-        'Feature3': range(20,28),
-        'Feature4': range(30,38),
-}
-
-st.dataframe(described_data)
+    st.dataframe(described_data)
